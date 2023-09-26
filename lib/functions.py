e65da5c61d5b9a329f1cd97184cd22e78041b6f4
Mary <omota4fidelity@gmail.com>
2023-09-26 14:12:15 +0100

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
+
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+greet("Mary")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Guido")
 
 def add(num1, num2):
     pass
+    return num1 + num2
+    
+add(45, 55)
 
-def halve(number):
+def halve(number): 
     pass
+    return number / 2
+
+halve(50)
+
